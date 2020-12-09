90172320e82e0ba6d1ae893f28066d2834c36cb3
KennyUTN <asisignacio@gmail.com>
2020-12-09 14:54:20 -0300

    Readme and final linters

--- main.rb
+++ main.rb
@@ -4,12 +4,12 @@ def bubble_sort(array)
     swap = false
     index = 0
 
-    (array.size - 1).times do |index|
+    (array.size - 1).times do
       if array[index] > array[index + 1]
         array[index], array[index + 1] = array[index + 1], array[index]
         swap = true
       end
-      index += 1
+      index = (index + 1)
     end
   end
 
@@ -20,19 +20,17 @@ bubble_sort([3, 4, 5, 2, 1])
 
 def bubble_sort_by(array)
   swap = true
-  if array.size >= 1
-    while swap == true
-      swap = false
-      (array.size - 1).times do |i|
-        comparator = yield array[i], array[i + 1]
-        if comparator == true
-          array[i], array[i + 1] = array[i + 1], array[i]
-          swap = true
-        end
+  while swap == true
+    swap = false
+    (array.size - 1).times do |i|
+      comparator = yield array[i], array[i + 1]
+      if comparator == true
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
       end
     end
-    print array
-end
+  end
+  print array
 end
 
 bubble_sort_by(%w[hey kenny test world hi]) do |left, right|
