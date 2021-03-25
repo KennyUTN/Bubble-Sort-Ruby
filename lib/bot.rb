4aa99a648b11513886f513019eb81b8beb4fa78a
KennyUTN <asisignacio@gmail.com>
2021-03-25 10:13:04 -0300

    Philosopher Bot Working

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,7 +4,30 @@ token = "1645105386:AAEkFx8umI3nEcBG5FUrGjr7zNaIUpkB6YY"
 
 Telegram::Bot::Client.run(token) do |bot|
 
+
+
   bot.listen do |msg|
-    bot.api.send_message(chat_id: message.chat.id, text: "Chupame 0la pija")
+    happyquote = ["Be yourself; everyone else is already taken",
+                  "To live is the rarest thing in the world. Most people exist, that is all.",
+                   "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+                   "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+                   "Do one thing every day that scares you"].sample
+
+    sadquote = ["Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate; only love can do that.",
+                "Our lives begin to end the day we become silent about things that matter.",
+                "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+                "I have not failed. I've just found 10,000 ways that won't work.",
+                "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time."].sample
+
+    message = "My fellow friend, send me a :) or :( so you can recieve a proper quote"
+
+    if msg.text == ':grinning:' || msg.text == ':slight_smile:' then message = happyquote
+    end
+
+    if msg.text == ':worried:' || msg.text == ':slight_frown:' then message = sadquote
+    end
+
+
+    bot.api.send_message(chat_id: msg.chat.id, text: message)
   end
 end
